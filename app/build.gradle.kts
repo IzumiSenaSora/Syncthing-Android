2d2ee2ed32a95bbee417f2c833810cd56f2e9aed
Izumi Sena Sora (UnOrdinary) <c588968a-3f54-48af-baa1-41a7a625bd4e@8shield.net>
2025-08-10 16:00:51 +0600

    update to 1.30.0
    M  app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.unordinary.syncthingandroid"
         minSdk = 21
         targetSdk = 33
-        versionCode = 4408
-        versionName = "1.29.7"
+        versionCode = 4409
+        versionName = "1.30.0"
         testApplicationId = "com.unordinary.syncthingandroid.test"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
