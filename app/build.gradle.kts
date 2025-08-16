92cb3b1985c24f568706e8e8a2b15f7d6f594870
Izumi Sena Sora (UnOrdinary) <c588968a-3f54-48af-baa1-41a7a625bd4e@8shield.net>
2025-08-16 12:33:47 +0600

    update to 2.0.2
    M  app/build.gradle.kts
    M  syncthing/src/github.com/syncthing/syncthing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.unordinary.syncthingandroid"
         minSdk = 21
         targetSdk = 33
-        versionCode = 4411
-        versionName = "2.0.1"
+        versionCode = 4412
+        versionName = "2.0.2"
         testApplicationId = "com.unordinary.syncthingandroid.test"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
