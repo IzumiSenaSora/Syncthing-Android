06d796db98000ed6ea953bb16964ae0636fa0562
Izumi Sena Sora (UnOrdinary) <c588968a-3f54-48af-baa1-41a7a625bd4e@8shield.net>
2025-11-04 18:03:55 +0600

    update to 2.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.unordinary.syncthingandroid"
         minSdk = 21
         targetSdk = 33
-        versionCode = 4420
-        versionName = "2.0.10"
+        versionCode = 4421
+        versionName = "2.0.11"
         testApplicationId = "com.unordinary.syncthingandroid.test"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
